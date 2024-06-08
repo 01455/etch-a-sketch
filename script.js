c86a16f741e23afd9fdcd7d127e51402a3cf13b8
01455 <95077343+01455@users.noreply.github.com>
2024-06-08 22:09:53 +0300

    add set squares btn

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 const containerDiv = document.querySelector('#container')
+const gridBtn = document.createElement('button')
+gridBtn.innerText = 'Set number of squares per side'
+gridBtn.classList.add('btn')
+containerDiv.insertAdjacentElement('beforebegin', gridBtn)
 
 function createDivs(num) {
+    
     const divSize = containerDiv.clientHeight / num
-    console.log(divSize)
+
+    console.log(`Calculated div size: ${divSize}px`);
+
+    containerDiv.innerHTML = '';
+
     for (let totalDivs = 1; totalDivs <= num * num; totalDivs++) {
         let div = document.createElement('div');
         div.classList.add('gridDiv');
@@ -11,5 +20,14 @@ function createDivs(num) {
         containerDiv.appendChild(div);
         console.log(`appended div nr ${totalDivs}`);
     }
-    return
 }
+
+function addListeners() {
+    const gridDivElements = document.querySelectorAll('.gridDiv')
+    for (divElement of gridDivElements) {
+        divElement.addEventListener('mouseover', (e) => {
+            e.currentTarget.style.backgroundColor = 'black';
+        })
+    }
+}
+
